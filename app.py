e824a8877f454f48b00fcbdb124bd5f3f0b488e0
Yoruroong <tuntun4132@naver.com>
2020-04-20 01:06:41 +0900

    fin?

---
+++ app.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import inspect
+import datetime
+
+app = commands.Bot(command_prefix = 'r!')
+
+TOKEN = ''
+
+@app.event
+async def on_ready():
+    print('Login!')
+
+@app.event
+async def on_message(message):
+    if message.author.bot: return
+    if message.content.startswith('r!c'):
+        if message.author.id == 480240821623455746:
+            msg=message.content
+            arg=msg.split(" ")
+            args=arg[1:]
+            msg = ''.join(args)
+            try:
+                python = '```py\n{}\n```'
+                res = eval(msg)
+                if inspect.isawaitable(res):
+                    result = await res
+                else:
+                    result = res
+            except Exception as e:
+                embed = discord.Embed(colour=0xef6767, timestamp=datetime.datetime.utcnow())
+                embed.add_field(name='Error', value=python.format(str(e)))
+                return await message.channel.send(embed=embed)
+
+            embed = discord.Embed(colour=0x6bffc8, timestamp=datetime.datetime.utcnow())
+            embed.add_field(name='Success', value=python.format(result))
+            await message.channel.send(embed=embed)
+
+app.run(TOKEN)
